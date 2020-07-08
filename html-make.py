e545cc4d2789d469812546dc9ace3e457030d97f
Iacchus Mercurius <kassivs.@gmail.com>
2020-07-08 04:31:57 -0300

    ok

--- html-make.py
+++ html-make.py
@@ -18,27 +18,11 @@ software = ['android', 'apache', 'archlinux', 'ardour', 'asciinema',
             'ubuntu', 'unity', 'vim', 'wechat', 'wikipedia', 'wordpress',
             'x-dot-org']
 
-#model_a = """
-#<!--
-#
-#{icon_name}
-#
-#-->
-#<div class="box">
-#  <div class="badge {title}-badge">
-#    <div class="left {title}-left"><img src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/{title}.svg" /></div>
-#    <div class="right {title}-right">{title}</div>
-#  </div>
-#</div>
-#
-#"""
 
 # let's get our badge models and register them locally
 for key, value in models.__dict__.items():
     if key.startswith('model_'):
-        #model_name = "".join(key.split('_', maxsplit=1)[1])
         model_name = key.split('_', maxsplit=1)[1]
-
         MODEL_REGISTRY.update({model_name: value})
 
 with open('simple-icons.json', 'r') as data:
@@ -46,21 +30,14 @@ with open('simple-icons.json', 'r') as data:
 
 our_html = str()
 
-print(MODEL_REGISTRY)
+#print(MODEL_REGISTRY)
 for model_name, model_str in MODEL_REGISTRY.items():
+
     our_html += "".join([model_str.format(icon_name=item['title'],
-                                         title=item['title'].lower())
+                                          title=item['title'].lower())
                        for item in icon_data['icons']
                        if item['title'].lower() in software])
 
-
-
-#for item in icon_data['icons']:
-#
-#    if item['title'].lower() in software:
-# 
-#        our_html += model_a.format(icon_name=item['title'],
-#                               title=item['title'].lower())
 
 print(our_html)
 
---
+++ models.py
@@ -0,0 +1,17 @@
+
+model_a = """
+<!--
+
+{icon_name}
+
+-->
+<div class="box">
+  <div class="badge {title}-badge">
+    <div class="left {title}-left"><img src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/{title}.svg" /></div>
+    <div class="right {title}-right">{title}</div>
+  </div>
+</div>
+
+"""
+
+
